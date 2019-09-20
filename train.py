a31896a85e2e41c98b3145c70b21afa03c8f72ef
Legolasy <43330865+Legolasy@users.noreply.github.com>
2019-09-20 15:36:45 +0800

    Update train.py
    用来训练点云语义分割（Semantic Segmentation in Scenes）的文件

--- train.py
+++ train.py
@@ -14,6 +14,8 @@ sys.path.append(os.path.join(BASE_DIR, 'utils'))
 import provider
 import tf_util
 
+
+#命令行解析。num_point默认4096，这与所用数据集有关。
 parser = argparse.ArgumentParser()
 parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
 parser.add_argument('--model', default='pointnet_cls', help='Model name: pointnet_cls or pointnet_cls_basic [default: pointnet_cls]')
@@ -28,7 +30,7 @@ parser.add_argument('--decay_step', type=int, default=200000, help='Decay step f
 parser.add_argument('--decay_rate', type=float, default=0.7, help='Decay rate for lr decay [default: 0.8]')
 FLAGS = parser.parse_args()
 
-
+#从命令行接受的全局变量。DECAY_STEP 和 DECAY_RATE用来计算learning_rate的衰减。
 BATCH_SIZE = FLAGS.batch_size
 NUM_POINT = FLAGS.num_point
 MAX_EPOCH = FLAGS.max_epoch
@@ -41,6 +43,8 @@ DECAY_RATE = FLAGS.decay_rate
 
 MODEL = importlib.import_module(FLAGS.model) # import network module
 MODEL_FILE = os.path.join(BASE_DIR, 'models', FLAGS.model+'.py')
+
+#记录训练日志文件。以及备份 model.py 和 train.py。
 LOG_DIR = FLAGS.log_dir
 if not os.path.exists(LOG_DIR): os.mkdir(LOG_DIR)
 os.system('cp %s %s' % (MODEL_FILE, LOG_DIR)) # bkp of model def
@@ -48,6 +52,9 @@ os.system('cp train.py %s' % (LOG_DIR)) # bkp of train procedure
 LOG_FOUT = open(os.path.join(LOG_DIR, 'log_train.txt'), 'w')
 LOG_FOUT.write(str(FLAGS)+'\n')
 
+#全局变量
+#40个语义分类标签 2048为每个样本点数 语义分割具体看https://blog.csdn.net/shaozhenghan/article/details/81087024
+#BN_ 开头的4个变量用来计算 Batch Normalization 的Decay参数，即decay参数也随着训练逐渐decay。
 MAX_NUM_POINT = 2048
 NUM_CLASSES = 40
 
@@ -58,18 +65,24 @@ BN_DECAY_CLIP = 0.99
 
 HOSTNAME = socket.gethostname()
 
+
+#分配训练集和测试集
 # ModelNet40 official train/test split
 TRAIN_FILES = provider.getDataFiles( \
     os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/train_files.txt'))
 TEST_FILES = provider.getDataFiles(\
     os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/test_files.txt'))
 
+#log训练日志
 def log_string(out_str):
     LOG_FOUT.write(out_str+'\n')
     LOG_FOUT.flush()
     print(out_str)
 
-
+#计算指数衰减的学习率
+#函数tf.train.exponential_decay(learning_rate, global_step, decay_steps, decay_rate, staircase=False, name=None)为指数衰减函数
+#计算公式为decayed_learning_rate = learning_rate * decay_rate ^ (global_step / decay_steps)
+#此处global_step= batch * BATCH_SIZE
 def get_learning_rate(batch):
     learning_rate = tf.train.exponential_decay(
                         BASE_LEARNING_RATE,  # Base learning rate.
@@ -80,6 +93,7 @@ def get_learning_rate(batch):
     learning_rate = tf.maximum(learning_rate, 0.00001) # CLIP THE LEARNING RATE!
     return learning_rate        
 
+#计算衰减的Batch Normalization 的 decay
 def get_bn_decay(batch):
     bn_momentum = tf.train.exponential_decay(
                       BN_INIT_DECAY,
@@ -90,7 +104,9 @@ def get_bn_decay(batch):
     bn_decay = tf.minimum(BN_DECAY_CLIP, 1 - bn_momentum)
     return bn_decay
 
+#训练函数train
 def train():
+    
     with tf.Graph().as_default():
         with tf.device('/gpu:'+str(GPU_INDEX)):
             pointclouds_pl, labels_pl = MODEL.placeholder_inputs(BATCH_SIZE, NUM_POINT)
@@ -102,7 +118,10 @@ def train():
             batch = tf.Variable(0)
             bn_decay = get_bn_decay(batch)
             tf.summary.scalar('bn_decay', bn_decay)
+            #这一段主要是placeholder，以及batch初始化为0
+            #batch如何增加？
 
+            
             # Get model and loss 
             pred, end_points = MODEL.get_model(pointclouds_pl, is_training_pl, bn_decay=bn_decay)
             loss = MODEL.get_loss(pred, labels_pl, end_points)
@@ -111,6 +130,9 @@ def train():
             correct = tf.equal(tf.argmax(pred, 1), tf.to_int64(labels_pl))
             accuracy = tf.reduce_sum(tf.cast(correct, tf.float32)) / float(BATCH_SIZE)
             tf.summary.scalar('accuracy', accuracy)
+            #pred为预测值 调用model.py 中的 get_model()得到 维度为B*N*40,40为channel数，对应40个分类标签
+            #40个值中最大的下标就是所预测的分类标签
+            
 
             # Get training operator
             learning_rate = get_learning_rate(batch)
@@ -120,15 +142,17 @@ def train():
             elif OPTIMIZER == 'adam':
                 optimizer = tf.train.AdamOptimizer(learning_rate)
             train_op = optimizer.minimize(loss, global_step=batch)
+            #获得衰减后的学习率，以及选择优化器optimizer。
+            
             
             # Add ops to save and restore all the variables.
             saver = tf.train.Saver()
             
         # Create a session
         config = tf.ConfigProto()
-        config.gpu_options.allow_growth = True
-        config.allow_soft_placement = True
-        config.log_device_placement = False
+        config.gpu_options.allow_growth = True#让tensorflow动态申请显存
+        config.allow_soft_placement = True#若GPU不存在自动降到CPU运行
+        config.log_device_placement = False#在终端打印出各项操作是在哪个设备上运行的。
         sess = tf.Session(config=config)
 
         # Add summary writers
@@ -165,6 +189,8 @@ def train():
             if epoch % 10 == 0:
                 save_path = saver.save(sess, os.path.join(LOG_DIR, "model.ckpt"))
                 log_string("Model saved in file: %s" % save_path)
+      #初始化参数，开始训练。
+      #train_one_epoch 函数用来训练一个epoch，eval_one_epoch函数用来每运行一个epoch后evaluate在测试集的accuracy和loss。每10个epoch保存1次模型。
 
 
 
@@ -182,9 +208,9 @@ def train_one_epoch(sess, ops, train_writer):
         current_data = current_data[:,0:NUM_POINT,:]
         current_data, current_label, _ = provider.shuffle_data(current_data, np.squeeze(current_label))            
         current_label = np.squeeze(current_label)
-        
+        #provider.shuffle_data 函数随机打乱数据，返回打乱后的数据。 
         file_size = current_data.shape[0]
-        num_batches = file_size // BATCH_SIZE
+        num_batches = file_size // BATCH_SIZE#计算在指定BATCH_SIZE下，训练1个epoch 需要几个mini-batch训练。
         
         total_correct = 0
         total_seen = 0
@@ -208,11 +234,12 @@ def train_one_epoch(sess, ops, train_writer):
             total_correct += correct
             total_seen += BATCH_SIZE
             loss_sum += loss_val
-        
+        #在一个epoch 中逐个mini-batch训练直至遍历完一遍训练集。计算总分类正确数total_correct和已遍历样本数total_senn，总损失loss_sum.
         log_string('mean loss: %f' % (loss_sum / float(num_batches)))
         log_string('accuracy: %f' % (total_correct / float(total_seen)))
+        #记录平均loss，以及平均accuracy。
 
-        
+#用来在测试集上评估evaluate。与train_one_epoch 类似。        
 def eval_one_epoch(sess, ops, test_writer):
     """ ops: dict mapping from string to tf ops """
     is_training = False
